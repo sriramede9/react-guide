bdad4002722f944f74ff589582000e4d2b4d56fa
sriram.ede <sriram.ede@loblaw.ca>
2022-03-20 11:19:55 -0400

    adding react classes dynamically

--- components/CourseGoals/CourseInput/CourseInput.js
+++ components/CourseGoals/CourseInput/CourseInput.js
@@ -9,6 +9,9 @@ const CourseInput = (props) => {
   const [isValid,setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
+    if(event.target.value.length >0){
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
@@ -24,8 +27,8 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
